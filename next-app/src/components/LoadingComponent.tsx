9b40467fb332b3ac646b5441952ff698c4a426f9
joseph-allen <joseph.allen.1994@hotmail.com>
2025-05-19 16:47:53 +0100

    Add Loading Component, tests, Storybook

---
+++ next-app/src/components/LoadingComponent.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Image from 'next/image';
+
+type LoadingComponentProps = {
+  text?: string;
+  withDots?: boolean;
+};
+
+export const LoadingComponent = ({
+  text = 'Loading...',
+  withDots = false,
+}: LoadingComponentProps) => {
+  const [dots, setDots] = useState(1);
+
+  // Animate dots every 2 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev < 4 ? prev + 1 : 1));
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <Image
+        src="/wine.apng"
+        alt="Loading - Animated icons by Lordicon.com"
+        width={200}
+        height={200}
+        style={{ marginBottom: '10px' }}
+        data-testid="loading-image"
+      />
+      <Typography variant="h4" sx={{ marginTop: 2 }}>
+        {text}
+        {withDots && (
+          <span data-testid="loading-ellipsis">{'.'.repeat(dots)}</span>
+        )}
+      </Typography>
+    </Box>
+  );
+};
+
+export default LoadingComponent;
